2f5c40d0d527ecc1ba8baa29343e52007e3605f8
Piotr Opyd <opydpiotrek@gmail.com>
2019-05-10 23:08:09 +0200

    controll commit

--- app/src/main/java/com/mobile/usoz/Calendar/Notes/SaveNoteActivity.java
+++ app/src/main/java/com/mobile/usoz/Calendar/Notes/SaveNoteActivity.java
@@ -2,11 +2,16 @@ package com.mobile.usoz.Calendar.Notes;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.mobile.usoz.R;
 
 public class SaveNoteActivity extends AppCompatActivity {
 
+    private Button mSaveButton;
+    private EditText mDateEditText;
+    private EditText mNoteContentEditText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
