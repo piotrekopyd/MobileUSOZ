e1f5871e917e5c6d5220721b7c748668ee51b20e
Piotr Opyd <opydpiotrek@gmail.com>
2019-06-09 21:18:35 +0200

    Fix delete notes issue

--- app/src/main/java/com/mobile/usoz/DatabaseManager/NotesRecyclerViewAdapterDatabaseManager.java
+++ app/src/main/java/com/mobile/usoz/DatabaseManager/NotesRecyclerViewAdapterDatabaseManager.java
@@ -4,6 +4,7 @@ import android.support.annotation.NonNull;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FieldValue;
@@ -44,14 +45,41 @@ public class NotesRecyclerViewAdapterDatabaseManager extends DatabaseManager imp
                         if(document.exists()){
                             ArrayList<String> list = (ArrayList<String>)document.get(KEY_NOTE);
                             if(list.isEmpty()){
-                                db.collection(NOTES_COLLECTION_PATH).document(user.getUid()).collection(month).document(KEY_NUMBERS_OF_DAY_DOCUMENT).update(KEY_NOTE, FieldValue.arrayRemove(day));
-                                callback.backToCalendarView(true);
+                                isAnyEvent(day, month, callback);
                             } else {
                                 callback.backToCalendarView(false);
                             }
                         } else {
                             callback.backToCalendarView(false);
                         }
+                    }
+                });
+    }
+    private void isAnyEvent(final String day, final String month, final NotesRecyclerViewAdapterDatabaseManagerDeleteDateInterface callback){
+        db.collection(NOTES_COLLECTION_PATH).document("Events").collection(month).document(day).get()
+                .addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                        DocumentSnapshot document = task.getResult();
+                        // If deleted note was last one at particular month we need to delete date from dates list at Firestore
+                        // Callback function takes us back to calendar activity
+                        if(document.exists()){
+                            ArrayList<String> list = (ArrayList<String>)document.get(KEY_NOTE);
+                            if(list.isEmpty()){
+                                db.collection(NOTES_COLLECTION_PATH).document(user.getUid()).collection(month).document(KEY_NUMBERS_OF_DAY_DOCUMENT).update(KEY_NOTE, FieldValue.arrayRemove(day));
+                                callback.backToCalendarView(true);
+                            } else {
+                                callback.backToCalendarView(false);
+                            }
+                        } else {
+                            callback.backToCalendarView(true);
+                        }
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        callback.backToCalendarView(true);
                     }
                 });
     }
