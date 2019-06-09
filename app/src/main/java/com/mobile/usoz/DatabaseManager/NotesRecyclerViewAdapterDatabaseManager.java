42f42a5b9c98faa9f2f3bafc2fceacb6012881f5
Piotr Opyd <opydpiotrek@gmail.com>
2019-06-09 22:23:04 +0200

    Fix display day in notes

--- app/src/main/java/com/mobile/usoz/DatabaseManager/NotesRecyclerViewAdapterDatabaseManager.java
+++ app/src/main/java/com/mobile/usoz/DatabaseManager/NotesRecyclerViewAdapterDatabaseManager.java
@@ -72,7 +72,7 @@ public class NotesRecyclerViewAdapterDatabaseManager extends DatabaseManager imp
                                 callback.backToCalendarView(false);
                             }
                         } else {
-                            db.collection(NOTES_COLLECTION_PATH).document(user.getUid()).collection(month).document(KEY_NUMBERS_OF_DAY_DOCUMENT).update(KEY_NOTE, FieldValue.arrayRemove(day));te
+                            db.collection(NOTES_COLLECTION_PATH).document(user.getUid()).collection(month).document(KEY_NUMBERS_OF_DAY_DOCUMENT).update(KEY_NOTE, FieldValue.arrayRemove(day));
                             callback.backToCalendarView(true);
                         }
                     }
